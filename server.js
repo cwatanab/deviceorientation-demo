12fb3e725b704a3164747685b8bf713acc247cb1
Chihiro WATANABE <chihiro.watanabe@live.jp>
2024-09-04 10:41:04 +0900

    ngrok を内蔵

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require("express");
-var app = express();
-var http = require("http").Server(app);
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const ngrok = require("@ngrok/ngrok");
 const io = require("socket.io")(http);
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 io.on("connection", function (socket) {
   console.log("connected");
@@ -17,6 +18,14 @@ io.on("connection", function (socket) {
   });
 });
 
-http.listen(PORT, function () {
+http.listen(PORT, async function () {
   console.log("Server listening. Port:" + PORT);
+  const listener = await ngrok.forward({
+    addr: 8000,
+    authtoken_from_env: true,
+    basic_auth: process.env.NGROK_AUTH,
+    region: "jp",
+    proto: "http",
+  });
+  console.log(`Ingress established at: ${listener.url()}`);
 });
